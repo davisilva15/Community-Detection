3b2f967bfffadcf6e5d7acef9f364fc6a1ebeaa8
davisilva15 <davisilva15@gmail.com>
2016-04-13 19:45:19 +0200

    Update and rename belief_propagation.py to BP_infer.py

--- List/BP_infer.py
+++ List/BP_infer.py
@@ -5,22 +5,16 @@ def BP_Inference(q, n, c, adj_list, criterium, t_max):
 	# Number of nodes on the graph
 	N = len(adj_list.keys())
 
-	# For each directed edge (u, v) we associate a code
+	# For each directed edge (u, v) we associate a code from 0 to 2|E| - 1
 	edge_to_code = {}
 	code_to_edge = []
-	# Interval of edge codes departing from each node
-	ext_edges = np.zeros((N, 2), dtype = np.int)
-
+	
 	# For each directed edge we associate a "message" array of size q
 	messages = []
-	# For each node we associate the marginal probability of it belonging to each group
-	marg_prob = np.zeros((N, q))
 
 	count = 0
 	# For each node of the graph
 	for u in adj_list.keys():
-		# First directed edge from u (if any) has code count
-		ext_edges[u - 1][0] = count
 		# For each of u's neighbors
 		for v in adj_list[u]:
 			# Each message is randomly initialized
@@ -28,20 +22,21 @@ def BP_Inference(q, n, c, adj_list, criterium, t_max):
 			edge_to_code[(u, v)] = count
 			code_to_edge.append((u, v))
 			count += 1
-		# Last directed edge from u has code count - 1
-		ext_edges[u - 1][1] = count
-		# Number of neighbors of node u
-		nb_neighbors = count - ext_edges[u - 1][0]
+	
+	# For each node we associate the marginal probability of it belonging to each group
+	marg_prob = np.zeros((N, q))
+	
+	for u in adj_list.keys():
+		# List of nodes adjacent to u
+		neighbors = adj_list[u]
 
 		# Compute the marginal probability array of node u
-		if nb_neighbors > 0:
-			p = np.zeros(q)
-			for i in range(ext_edges[u - 1][0], count):
-				p += messages[i]
-			p /= nb_neighbors
-		else:
-			p = np.ones(q)/p
-		marg_prob[u - 1] = p
+		p = marg_prob[u - 1]
+		for v in neighbors:
+			p += np.dot(c, messages[edge_to_code[(v, u)]])*messages[edge_to_code[(u, v)]]
+		if len(neighbors) == 0:
+			np.copyto(p, np.ones(q))
+		p /= sum(p)
 
 	# Initialization of the "external field" h
 	h = np.zeros(q)
@@ -49,10 +44,16 @@ def BP_Inference(q, n, c, adj_list, criterium, t_max):
 		h += np.dot(c, marg_prob[k])
 	h /= N
 
+	# Measures the convergence of the messages
 	conv = criterium + 1
+	# Number of steps taken by the algorithm
 	t = 0
+	# All codes of the directed edges
 	arr = np.arange(count)
+
+	prod = np.zeros(q)
 	old_message = np.zeros(q)
+	old_marg_prob = np.zeros(q)
 	while conv > criterium and t < t_max:
 		t += 1
 		conv = 0
@@ -65,7 +66,7 @@ def BP_Inference(q, n, c, adj_list, criterium, t_max):
 			np.copyto(old_message, messages[i])
 			
 			# Updating message code i
-			prod = np.ones(q)
+			np.copyto(prod, np.ones(q))
 			for k in adj_list[u]:
 				if k != v:
 					prod *= np.dot(c, messages[edge_to_code[(k, u)]])
@@ -73,10 +74,10 @@ def BP_Inference(q, n, c, adj_list, criterium, t_max):
 			messages[i] /= sum(messages[i])
 
 			# Adding to conv the difference of new to the old message code i
-			conv += np.linalg.norm(messages[i] - old_message)
+			conv += np.linalg.norm(messages[i] - old_message, 1)
 
 			# The marginal probability array of node v before changes are made
-			old_marg_prob = marg_prob[v - 1]
+			np.copyto(old_marg_prob, marg_prob[v - 1])
 			# Updating the marginal probability array of node v
 			marg_prob[v - 1] *= np.dot(c, messages[i])/np.dot(c, old_message)
 			marg_prob[v - 1] /= sum(marg_prob[v - 1])
@@ -84,41 +85,58 @@ def BP_Inference(q, n, c, adj_list, criterium, t_max):
 			# Updating the external field h
 			h += np.dot(c, marg_prob[v - 1] - old_marg_prob)/N
 
+	# Updating the array n
+	np.copyto(n, np.sum(marg_prob, axis = 0)/N)
+
 	# The belief propagation estimate for the free energy
-	f_BP = 0
-	for u in adj_list.keys():
-		prod_u = n*np.exp(-h)
-		for v in adj_list[u]:
-			Zuv = 0
-			uv = edge_to_code[(u, v)]
-			vu = edge_to_code[(v, u)]
-			prod_u *= np.dot(c, messages[vu])
-			for a in range(q):
-				Zuv += c[a][a]*messages[uv][a]*messages[vu][a]
-				for b in range(a + 1, q):
-					Zuv += c[a][b]*(messages[uv][a]*messages[vu][b] + messages[uv][b]*messages[vu][a])
-			f_BP += np.log(Zuv)
-		f_BP -= 2*np.log(sum(prod_u))
-	f_BP /= (2*N)
+	f_BP = free_energy(adj_list, N, q, n, c, h, messages, edge_to_code)
 
-	# The average (directed) degree
-	c_avg = 0
-	for a in range(q):
-		for b in range(q):
-			c_avg += c[a][b]*n[a]*n[b]
+	# An array containing the most probable group for each node
+	groups = np.argmax(marg_prob, axis = 1) + np.ones(N, dtype = np.int)
 
-	# Final value of the Bethe free energy
-	f_BP -= c_avg/2
-
-	
-	groups = np.zeros(N, dtype = np.int)
-	for u in range(N):
-		groups[u] = np.argmax(marg_prob[u]) + 1	
-
-	return groups
+	return groups, f_BP
 
 
 def rand_init(q):
 	# A random array of size q with positive elements whose sum is 1
 	r = np.random.rand(q)
 	return r/sum(r)
+
+
+def free_energy(adj_list, N, q, n, c, h, messages, edge_to_code):
+	# Calculates the free energy associated to the parameters given
+	f_BP = 0
+	prod = n*np.exp(-h)
+	prod_u = np.zeros(q)
+	cmvu = np.zeros(q)
+	c_new = np.zeros((q, q))
+	
+	# Only application of formulas from statistical physics
+	for u in adj_list.keys():
+		np.copyto(prod_u, prod)
+		for v in adj_list[u]:
+			uv = edge_to_code[(u, v)]
+			vu = edge_to_code[(v, u)]
+
+			# All needed to compute the free energy
+			np.copyto(cmvu, np.dot(c, messages[vu]))
+			prod_u *= cmvu
+			Zuv = np.dot(np.transpose(messages[uv]), cmvu)
+			# Needed to update the matrix c
+			c_new += np.dot(messages[uv], np.transpose(messages[vu]))/Zuv
+
+			f_BP += np.log(Zuv)
+		f_BP -= 2*np.log(sum(prod_u))
+	f_BP /= (2*N)
+
+	nn = np.dot(n, np.transpose(n))
+	c *= c_new/N
+	# Average (directed) degree
+	c_avg = np.sum(c)
+	# Updated value of array c
+	c /= nn
+
+	# Final value of the Bethe free energy
+	f_BP -= c_avg/2
+
+	return f_BP
