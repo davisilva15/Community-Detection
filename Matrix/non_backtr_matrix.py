68878faa5d887fdf7ce90ad8050cb3342d7da480
davisilva15 <davisilva15@gmail.com>
2016-04-18 19:48:08 +0200

    Update non_backtr_matrix.py

--- Matrix/non_backtr_matrix.py
+++ Matrix/non_backtr_matrix.py
@@ -7,26 +7,27 @@ def non_backtracking(adj_matrix):
 	Time: O(|E|^2)		Space: O(|E|^2)
 	"""
 	# Number of nodes on the graph
-	n = len(adj_matrix)
+	N = len(adj_matrix)
 
-	# Dictionary giving for each (directed) edge a number from 0 to 2|E| - 1
-	edge_dict = {}
+	# Maps linking each (directed) edge of the graph and a code from 0 to 2|E| - 1
+	edge_to_code = {}
+	code_to_edge = []
 	count = 0
-	for r in range(1, n):
-		for c in range(r + 1, n + 1):
-			if adj_matrix[r - 1][c - 1]:
-				edge_dict[(r, c)] = count
-				edge_dict[(c, r)] = count + 1
-				count += 2
+	for u in range(1, N + 1):
+		for v in range(1, N + 1):
+			if adj_matrix[u - 1][v - 1]:
+				edge_to_code[(u, v)] = count
+				code_to_edge.append((u, v))
+				count += 1
 	
 	# Construction of the non-backtracking matrix for this graph
-	non_backtr = np.zeros((count, count), dtype = np.int)
-	for i in range(1, n + 1):
-		for j in range(1, n + 1):
-			if adj_matrix[i - 1][j - 1]:
-				for k in range(1, n + 1):
-					if adj_matrix[j - 1][k - 1] and i != k:
-						non_backtr[edge_dict[(i, j)]][edge_dict[(j, k)]] = 1
+	non_backtr = np.zeros((count, count), dtype = np.int8)
+	for u in range(1, N + 1):
+		for v in range(1, N + 1):
+			if adj_matrix[u - 1][v - 1]:
+				for w in range(1, N + 1):
+					if adj_matrix[v - 1][w - 1] and u != w:
+						non_backtr[edge_to_code[(u, v)]][edge_to_code[(v, w)]] = 1
 	
 	# Returns the non-backtracking matrix (numpy int matrix) and edge dictionary
-	return non_backtr, edge_dict
+	return non_backtr, code_to_edge
