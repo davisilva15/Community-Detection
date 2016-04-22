ee569caf56f599cbe0a25f154f9744cc3c302668
davisilva15 <davisilva15@gmail.com>
2016-04-22 16:29:42 +0200

    Update BP_learn.py

--- List/BP_learn.py
+++ List/BP_learn.py
@@ -3,9 +3,14 @@ from BP_infer import BP_Inference
 
 
 def BP_learning(q, n_init, c_init, adj_list, crit_infer, crit_learn, tmax_infer, tmax_learn):
+	"""
+	Given a graph by its adjacency list and a number of groups to distribute its edges, along with initial assumptions of the
+	group parameters, runs the belief propagation algorithm to learn the true parameters and returns the most probable group
+	assignment and its free energy
+	"""
 	# Number of nodes on the graph
 	N = len(adj_list.keys())
-	
+
 	# Initial values of arrays n and c
 	n = n_init
 	c = c_init
@@ -14,14 +19,15 @@ def BP_learning(q, n_init, c_init, adj_list, crit_infer, crit_learn, tmax_infer,
 	t = 0
 	while conv > crit_learn and t < tmax_learn:
 		t += 1
-		
+
 		# Values of the arrays n and c before changes are made
 		n_old = np.copy(n)
 		c_old = np.copy(c)
-		
+
 		# Most probable group of each node and the free energy of this configuration
-		groups, f_BP = BP_Inference(q, n, c, adj_list, crit_infer, tmax_infer)
+		n, c, groups, f_BP = BP_Inference(q, n, c, adj_list, crit_infer, tmax_infer)
 		
 		# Updating the convergence measure
-		conv = np.linalg.norm(n - n_old, 1) + np.sum(np.fabs(c - c_old))
+		conv = np.sum(np.fabs(n - n_old)) + np.sum(np.fabs(c - c_old))
+	# Returns the estimated assignment of the nodes and its Bethe free energy
 	return groups, f_BP
